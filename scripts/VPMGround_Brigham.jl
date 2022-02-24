fc338db72c0901fd871b5266d179b559932ca63c
rymanderson <brigham.ostergaard@gmail.com>
2022-02-24 12:45:19 -0700

    changed angle definition again

--- scripts/VPMGround_Brigham.jl
+++ scripts/VPMGround_Brigham.jl
@@ -317,12 +317,11 @@ RPMref          = 1600;
 t_per_rev       = 60/RPMref;      #60s / rotations per minute -> s/rotation
 nrevs           = 40;
 ttot            = nrevs*t_per_rev
-t = 0:t_per_rev:ttot
-maneuver = generate_maneuver_windcraft_kinematic(nrevs)
-
+t               = 0:t_per_rev:ttot;
+generate_maneuver_windcraft_kinematic(nrevs);
 Vmean           = 2*pi*R/(nrevs*t_per_rev) # (m/s) mean velocity along a full circle
 Vref            = Vmean    #determine from paper?
-angle           = maneuver.(anglevehicle(t),);
+angle           = (anglevehicle(t),);
 # anglevehicle    = 0;
 my_angle_function(t) = zeros(3);
 anglevehcile = (my_angle_function,)
